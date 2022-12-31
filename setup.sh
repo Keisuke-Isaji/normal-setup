1097360ab1be5762ae5b4ba3757bf3bf4b7380a4
Keisuke Isaji <keisuke_isaji@mail.toyota.co.jp>
2022-12-31 15:39:01 +0900

    Deleted unused files

--- setup.sh
+++ setup.sh
@@ -2,9 +2,4 @@ cd scripts || exit
 source ./config-osx.sh
 source ./homebrew.sh
 source ./oh-my-zsh.sh
-source ./git.sh
 source ./dev-utilities.sh
-source ./ide.sh
-source ./node.sh
-source ./python.sh
-source ./ssh-key.sh
